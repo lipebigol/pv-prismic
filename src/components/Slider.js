73ee867dddda05dcfee8722f51be129e7de62be8
Felipe <lipebigol@gmail.com>
2021-06-21 17:38:33 -0300

    maxh slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -15,13 +15,13 @@ export default function SimpleSlider({ product }) {
     return (
         <Slider {...settings}>
             <div>
-                <Image src={product.heroSlider_1} maxW="500px" w="100%" />
+                <Image src={product.heroSlider_1} maxW="500px" w="100%" maxH="400px" h="100%" objectFit="contain" />
             </div>
             <div>
-                <Image src={product.heroSlider_2} maxW="500px" w="100%" />
+                <Image src={product.heroSlider_2} maxW="500px" w="100%" maxH="400px" h="100%" objectFit="contain" />
             </div>
             <div>
-                <Image src={product.heroSlider_3} maxW="500px" w="100%" />
+                <Image src={product.heroSlider_3} maxW="500px" w="100%" maxH="400px" h="100%" objectFit="contain" />
             </div>
 
         </Slider>
