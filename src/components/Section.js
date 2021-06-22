26ee0a21b94e25e792b835d57fb48fde408e18ab
Felipe <lipebigol@gmail.com>
2021-06-22 18:40:49 -0300

    adicionado favicon e arrumado tamanho da imagem na section

--- src/components/Section.js
+++ src/components/Section.js
@@ -7,7 +7,7 @@ export default function Section({ product }) {
                 return (
                     <Flex direction="column" textAlign="center" justify="center" color="gray.700" key={product.sectionTitle}>
                         <VStack my="6">
-                            <Image src={product.sectionImage} w="900px" maxH={["", "", "300px"]} objectFit="contain" mb="2" />
+                            <Image src={product.sectionImage} w="900px" maxH={["", "", "500px"]} objectFit="contain" mb="2" />
                             <Heading fontSize="2xl">{product.sectionTitle}</Heading>
                             <Text fontSize="xl" fontWeight="bold">{product.sectionSub}</Text>
                             <Text fontSize="lg">{product.sectionDescription}</Text>
